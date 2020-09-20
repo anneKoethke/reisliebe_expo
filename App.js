d352d2141a34b9df66db1b856f7381439e52c6b6
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-20 17:52:48 +0200

    fixed AppNavigator import

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { globalStyles } from './styles/global';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
-import AppNavigator from './routes/AppNavigator';
+import { AppNavigator } from './routes/AppNavigator';
 
 const getFonts = () => Font.loadAsync({
-  // 'permanent-marker': require('./assets/fonts/Permanent_Marker/PermanentMarker-Regular.ttf'),
   'spicy-rice': require('./assets/fonts/SpicyRice-Regular.ttf'),
   'dosis-regular': require('./assets/fonts/Dosis-Regular.ttf'),
   'dosis-light': require('./assets/fonts/Dosis-Light.ttf'),
-  'dosis-extra-light': require('./assets/fonts/Dosis-ExtraLight.ttf'),
+  'dosis-extra-light': require('./assets/fonts/Dosis-ExtraLight.ttf')
 });
 
 export default function App() {
