fa687eca98a81a86dc3b7e96f5e6ccf01f66312e
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-20 17:32:29 +0200

    font loading added

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from './styles/global';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import AppNavigator from './routes/AppNavigator';
+
+const getFonts = () => Font.loadAsync({
+  // 'permanent-marker': require('./assets/fonts/Permanent_Marker/PermanentMarker-Regular.ttf'),
+  'spicy-rice': require('./assets/fonts/SpicyRice-Regular.ttf'),
+  'dosis-regular': require('./assets/fonts/Dosis-Regular.ttf'),
+  'dosis-light': require('./assets/fonts/Dosis-Light.ttf'),
+  'dosis-extra-light': require('./assets/fonts/Dosis-ExtraLight.ttf'),
+});
 
 export default function App() {
-  return (
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.screenTitleText}>Reisliebe: Reiskochen für Anfänger</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded) {
+    return <AppNavigator />;
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    );
+  }
 }
