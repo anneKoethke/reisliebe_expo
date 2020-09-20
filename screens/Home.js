2f9c1f5cc4fd046b909f4e97dcb04591a23000a7
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-20 16:02:37 +0200

    basic structure of the app

---
+++ screens/Home.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+
+  // Liste aller Sorten (FlatList?)
+  const [riceData, setRiceData] = useState([/* Reisdatensatz */]);
+  // TODO: State für die Suche
+
+  return(
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.screenTitleText}>HomeScreen</Text>
+      <Text style={globalStyles.screenParagraph}>Suchfunktion um schneller den richtigen Reis zu finden (Modal?)</Text>
+      <Text style={globalStyles.screenParagraph}>Übersicht über die verschiedenen Reissorten und deren Zubereitung</Text>
+      <Text style={globalStyles.screenParagraph}>Details um Mengen zu ändern?</Text>
+    </View>
+  )
+}
---
+++ screens/World.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function World() {
+  return(
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.screenTitleText}>WorldScreen</Text>
+      <Text style={globalStyles.screenParagraph}>Weltkarte mit den Anbaugebieten</Text>
+      <Text style={globalStyles.screenParagraph}>Welche Sorte kommt woher?</Text>
+      <Text style={globalStyles.screenParagraph}>1. Land anklicken: Details mit Reissorten (Verknüpfung zu Details mit Zubereitung?</Text>
+      <Text style={globalStyles.screenParagraph}>2. Sorte auswählen -> Anbauländer werden gehighlighted</Text>
+    </View>
+  )
+}
