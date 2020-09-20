aeaf5391bb3dd3ccb64cbc0d99d2e52ab3f9c4de
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-20 16:17:33 +0200

    added navigtion, basic routes and About

---
+++ routes/AppNavigator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeStack from './HomeStack';
+import WorldStack from './WorldStack'; 
+import AboutStack from './AboutStack';
+
+const { Navigator, Screen } = createDrawerNavigator();
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <Navigator initialRouteName="HomeStack">
+      <Screen name="Home" component={HomeStack} />
+      <Screen name="World" component={WorldStack} />
+      <Screen name="About" component={AboutStack} />
+    </Navigator>
+  </NavigationContainer>
+);
