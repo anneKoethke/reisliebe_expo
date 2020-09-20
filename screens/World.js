19949f34ee9f3e23fa24621b65a2b88ef0da0820
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-20 17:57:37 +0200

    fiexed globalStyles

--- screens/World.js
+++ screens/World.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
 
 export default function World() {
   return(
--- styles/global.js
+++ styles/global.js
@@ -3,14 +3,16 @@ import { StyleSheet } from 'react-native';
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   screenTitleText: {
-    
+    fontFamily: 'spicy-rice',
+    fontSize: 20
   },
   screenParagraph: {
-
+    fontFamily: 'dosis-extra-light',
+    fontSize: 20
   }
 });
